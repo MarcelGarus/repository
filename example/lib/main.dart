ccb8bdabf51393d24e5b50f157d09da5c6126246
Marcel Garus <marcel.garus@gmail.com>
2019-10-02 15:58:26 +0200

    use repository_hive in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:hive/hive.dart';
 import 'package:repository/repository.dart';
+import 'package:repository_hive/repository_hive.dart';
 
 void main() async {
+  await Hive.init('.');
   doStuff();
   await Future.delayed(Duration(seconds: 1));
 }
@@ -9,20 +12,14 @@ Future<void> doStuff() async {
   print('Doing stuff');
   Repository<int> storage = CachedRepository<int>(
     strategy: CacheStrategy.onlyFetchFromSourceIfNotInCache,
-    cache: InMemoryStorage(),
+    cache: HiveRepository('threes'),
     source: ThreeGenerator(),
   );
-  //storage = ThreeGenerator();
 
   var id = const Id<int>('anything');
 
   var stream = storage.fetch(id);
   print('Fetching from stream $stream.');
-  /*var future = stream.first;
-  print('First future is $future');
-  var first = await future;
-  print('First is $first');*/
-  //stream.listen((item) => print('Stream yielded $item.'));
   print(await stream.first);
 
   print('Fetching again.');
