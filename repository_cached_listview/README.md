362c665d4b6ff87c94b23c4b59ea2434ef96e623
Marcel Garus <marcel.garus@gmail.com>
2019-11-07 11:53:40 +0100

    feat: initialize repository_cached_listview

---
+++ repository_cached_listview/README.md
@@ -0,0 +1,4 @@
+# repository_cached_listview
+
+A ListView that displays data from a `CachedRepository`. Supports
+pull-to-refresh, displaying banners etc.
---
+++ repository_cached_listview/test/repository_cached_listview_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:repository_cached_listview/repository_cached_listview.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+}
